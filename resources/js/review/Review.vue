12d0b27acee474d45fbfa79d84b7bb3aa4fb9bda
Patrick Wong <saihowong81@hotmail.com>
2021-01-26 23:21:26 +0000

    section 8 fetching booking by review key and creating resources to display selected info -79

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,18 +1,28 @@
 <template>
-   <div class="">
-      <div class="form-group">
-         <label for="" class="text-muted">Select the star rating(1 is worst - 5 is best)</label>
-         <star-rating  
-            class="fa-2x"
-            v-model="review.rating"
-         ></star-rating>
-      </div>
-      <div class="form-group">
-         <label for="content" class="text-muted">Describe your experience with:</label>
-         <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+   <div>
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+         <div v-if="alreadyReviewed">
+         <h3>You've already left a review</h3>
+         </div>
+         
+         <div v-else>
+            <div class="form-group">
+               <label for="" class="text-muted">Select the star rating(1 is worst - 5 is best)</label>
+               <star-rating  
+                  class="fa-2x"
+                  v-model="review.rating"
+               ></star-rating>
+            </div>
+            <div class="form-group">
+               <label for="content" class="text-muted">Describe your experience with:</label>
+               <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+            </div>
+            
+            <button class="btn btn-lg btn-primary btn-block">Submit</button>
+         </div>
       </div>
       
-      <button class="btn btn-lg btn-primary btn-block">Submit</button>
    </div>
 </template>
 
@@ -23,13 +33,29 @@
             review: {
                rating: 0,
                content: null
-            }
-         }
+            },
+            existingReview: null,
+            loading: false
+         };
       },
-      create() {
+      created() {
+         this.loading = true;
          //1. If review already exists (in reviews table by id)
+         axios
+         .get(`/api/reviews/${this.$route.params.id}`)
+         .then(response => (this.existingReview = response.data.data))
+         .catch(err => {
+            //
+         })
+         .then(() => (this.loading = false));
+
          //2. Fetch a booking by a review key
          //3. Store the review
+      },
+      computed: {
+         alreadyReviewed() {
+            return this.existingReview !== null;
+         }
       }
    };
 </script>
