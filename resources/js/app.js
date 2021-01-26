ce9a83c62a009568bb35541f71d736a7f9782b2b
Patrick Wong <saihowong81@hotmail.com>
2021-01-26 14:28:44 +0000

    section 7 completed. Review star rating implemented. Calculating how to display full, half and empty stars.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import Index from "./Index";
 import router from "./routes";
 import moment from "moment";
 
+import StarRating from "./shared/components/StarRating";
+
 window.Vue = require('vue');
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 // Vue.component('example-2', require('./components/Example2.vue').default);
@@ -12,6 +14,8 @@ window.Vue = require('vue');
 Vue.use(VueRouter);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
+
+Vue.component("star-rating", StarRating);
 
 const app = new Vue({
     el: '#app',
---
+++ resources/js/shared/components/StarRating.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="d-flex">
+
+      <i class="fas fa-star" v-for="star in fullStars" :key="'full' + star"></i>
+      <i class="fas fa-star-half-alt" v-if="halfStar"></i>
+      <i class="far fa-star" v-for="star in emptyStars" :key="'empty' + star"></i>
+   </div>
+</template>
+
+<script>
+   export default {
+      props: {
+         rating: Number
+      },
+      computed: {
+         halfStar() {
+            const fraction = Math.round(
+               (this.rating - Math.floor(this.rating)) * 100 
+            );
+            // console.log(fraction);
+            
+            return fraction > 0 && fraction <50;
+         },
+         fullStars() {
+            // >4.5 = 5stars
+            // 4.3 = 4.5stars
+            return Math.round(this.rating);
+         },
+         emptyStars() {
+            //if rating would be 1.9, ceil(1.9)= 2. 5 - 2 = 3. Renders 3 empty stars.
+            return 5 - Math.ceil(this.rating);
+         }
+      },
+   };
+</script>
